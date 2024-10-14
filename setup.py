1478cb5e2a654e80beffaeac8b5720a57a5f10fb
yt <yt@kt.io>
2024-10-14 20:16:04 +0100

    building / packaging py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cross-project-cluck",              
+    version="1.0.0",                 
+    description="CLI tool to identify cross project repo access in Azure DevOps.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="ytimyno",
+    author_email="ytimyno@gmail.com",
+    url="https://github.com/ytimyno/cross-project-cluck",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    license="GPLv3",
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
